3bd9cf95a8912a87c433bc34cdb153945a589b32
Nqobile sibiya <nqobilesibiya025@gmail.com>
2026-09-17 09:21:41 +0200

    orde delivery service

---
+++ oms_deliveryservice/src/main/java/com/fnb/oms_deliveryservice/OmsDeliveryserviceApplication.java
@@ -0,0 +1,13 @@
+package com.fnb.oms_deliveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmsDeliveryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmsDeliveryserviceApplication.class, args);
+	}
+
+}
---
+++ oms_deliveryservice/src/test/java/com/fnb/oms_deliveryservice/OmsDeliveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fnb.oms_deliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsDeliveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
